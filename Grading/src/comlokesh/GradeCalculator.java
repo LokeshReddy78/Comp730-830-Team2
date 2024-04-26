2127e952199d4e271f66838e3e11fae2b00bee85
katharynh <kh1197@usnh.edu>
2024-04-26 14:07:25 -0400

    content pane

--- Grading/src/comlokesh/GradeCalculator.java
+++ Grading/src/comlokesh/GradeCalculator.java
@@ -136,6 +136,7 @@ public class GradeCalculator extends JFrame {
 		 JLabel lblNewLabel_5 = new JLabel("Grade");
 		 lblNewLabel_5.setFont(new Font("Times New Roman", Font.BOLD, 14));
 		 lblNewLabel_5.setBounds(10, 221, 46, 14);
+		 contentPane.add(lblNewLabel_5);
 
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
